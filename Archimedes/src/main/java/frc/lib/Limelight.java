6f1819254e5c1b91d36f9b5595b27591d408bf5c
Mohid Rattu <59671390+MohidTheMan@users.noreply.github.com>
2023-02-11 12:19:23 -0500

    Combined Swerve2023 with ArmLidarMotionProfiling branch

--- Archimedes/src/main/java/frc/lib/Limelight.java
+++ Archimedes/src/main/java/frc/lib/Limelight.java
@@ -1,7 +1,14 @@
 package frc.lib;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -9,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * 
  * @author Dan Waxman
  * @author Shreyas Prasad
+ * @author Ty McKeon
 */
 public class Limelight {
 	
@@ -17,7 +25,7 @@ public class Limelight {
 	private final String mTableName;
 
 	public final static double kDefaultValue = 9999.9;
-
+	
 	/**
 	 * Creates a new Limelight Object
 	 * @param tableName The name of the Limelight's NetworkTable
@@ -110,6 +118,86 @@ public class Limelight {
 		return getValue("tl").getDouble(kDefaultValue);
 	}
 
+
+/**
+	 * 
+	 * Gets the robots pose in field space space as computed by solvepnp (x,y,z,rx,ry,rz).
+	 * <p>
+	 * Will return null if no targets are found.
+	 * <p>
+	 * Requires a third part JSON library https://mvnrepository.com/artifact/org.json/json
+	 * 
+	 * @return Matrix of Robot Pose in field space for each target
+	 * <p>
+	 * <li> Null if no targets found
+	 */
+	public FluidicalPoseInfo getBotPose() {
+		try {
+			String rawJSON = getValue("json").getString(""); //get the JSON dump from NetworkTables
+			if (rawJSON.isEmpty()) return null;
+
+			JSONObject json = new JSONObject(rawJSON);
+			JSONObject jsonResults = json.getJSONObject("Results");
+			boolean validTarget = jsonResults.getInt("v") == 1;
+			double timestamp = jsonResults.getDouble("ts");
+
+			Alliance alliance = DriverStation.getAlliance();
+			String allianceSidePose;
+
+			if(alliance == Alliance.Blue) {
+				allianceSidePose = "botpose_wpiblue";
+			} else if(alliance == Alliance.Red) {
+				allianceSidePose = "botpose_wpired";
+			} else {
+				throw new Exception("Alliance not Found!");
+			}
+
+			JSONArray poseArray = jsonResults.getJSONArray(allianceSidePose);
+
+			Pose2d fieldRelativePose = new Pose2d(
+					poseArray.getDouble(0), poseArray.getDouble(1),
+					new Rotation2d(poseArray.getDouble(3), poseArray.getDouble(4))
+			);
+	
+			return new FluidicalPoseInfo(fieldRelativePose, validTarget, timestamp);
+		} catch (Exception e) {
+			DriverStation.reportError("Grave error with limelight parsing", e.getStackTrace());
+		}
+		
+		return null;
+
+	}
+
+	public FluidicalPoseInfo getBotPoseViaNetworkTables() {
+		
+		try {
+			Alliance alliance = DriverStation.getAlliance();
+			if (alliance == Alliance.Invalid) throw new Exception("Alliance not Found!");
+
+			String allianceSidePose = alliance == Alliance.Blue ? "botpose_wpiblue" : "botpose_wpired";
+			double[] result = getValue(allianceSidePose).getDoubleArray(new double[]{});
+
+			double ts = getValue("ts").getDouble(-1);
+			int tv = (int) getValue("tv").getInteger(0l);
+			
+			if (ts == -1) throw new Exception("No timestamp found");
+			if (result.length == 0) throw new Error("No targets found");
+
+			boolean isValid = tv == 1;
+
+			Pose2d fieldRelativePose = new Pose2d(
+				result[0], result[1], 
+				new Rotation2d(result[3], result[4])
+			);
+
+			return new FluidicalPoseInfo(fieldRelativePose, isValid, ts);
+		} catch (Exception e) {
+			DriverStation.reportError("Gave error with Limelight Pose", e.getStackTrace());
+		}
+		
+		return null;
+	}
+
 	/**
 	 * Sets LED mode of Limelight.
 	 * 
@@ -161,5 +249,36 @@ public class Limelight {
 		SmartDashboard.putNumber(mTableName + " ta", getTa());
 		SmartDashboard.putNumber(mTableName + " ts", getTs());
 		SmartDashboard.putNumber(mTableName + " tl", getTl());
+	}
+
+	public class FluidicalPoseInfo {
+
+		private Pose2d mPose2d;
+		private boolean mValidTarget;
+		private double mTimestampSeconds;
+
+		public FluidicalPoseInfo(Pose2d pose, boolean validTarget, double timestampMs) {
+			mPose2d = pose;
+			mValidTarget = validTarget;
+			mTimestampSeconds = timestampMs/1000.0; //converting time to seconds
+		}
+
+		public double getTimestamp() {
+			return mTimestampSeconds;
+		}
+
+		public Pose2d getPose() {
+			return mPose2d;
+		}
+
+		public boolean isValidTarget() {
+			return mValidTarget;
+		}
+
+		@Override
+		public String toString() {
+			String prefix = mValidTarget ? "" : "[X]";
+			return prefix + mTimestampSeconds + ": " + mPose2d.toString();
+		}
 	}
 }
--- Archimedes/src/main/java/frc/robot/Constants.java
+++ Archimedes/src/main/java/frc/robot/Constants.java
@@ -16,5 +16,51 @@ public class Constants {
         public static final double kMaxAcceleration = 0.75;
 
     }
+    public static class DriveConstants {
+
+        public static enum CardinalDirection {
+            eX, eY
+        }
+
+        public static final double kDriveControllerDeadband = 0.05;
+        public static final boolean kSquareAxis = true; 
+
+        public static final double[] kDriveAngularGains = {0.02, 0.0, 0.0}; //dont use I it sucks - Shreyas
+        public static final double[] kDriveLinearGains = {0.4, 0.0, 0.0};
+
+        public static final double kPX = 1.25;
+        public static final double kPY = 1.25;
+
+        public static final double kAutoRotateEpsilon = 3.0;
+        public static final double kLinearDriveEpsilon = 0.0;
+        
+        
+        public static final double kDrivetrainTrackwidthMeters = 0.5461;
+        public static final double kDrivetrainWheelbaseMeters = 0.5461;
+        
+        // angles in radians. 
+        // to convert from degrees to radians multiply by pi/180 
+        public static final double kFrontLeftSteerOffset = -0.35 - (Math.PI / 2.0);//-.35;
+        public static final double kFrontRightSteerOffset = 0.4 - (Math.PI / 2.0);//0.40;
+        public static final double kBackLeftSteerOffset = 0.45 - (Math.PI / 2.0);//.45;
+        public static final double kBackRightSteerOffset = -0.5 - (Math.PI / 2.0);//-.5;
+
+
+        private static final double kMk4L1DriveReduction = (14.0 / 50.0) * (25.0 / 19.0) * (15.0 / 45.0);
+        private static final double kMk4WheelDiameter = 0.10033;
+
+        
+        public static final double kMaxVelocityMetersPerSecond = 6380.0 / 60.0 *
+            kMk4L1DriveReduction * kMk4WheelDiameter * Math.PI;
+
+        //TODO: Change
+        public static final double kMaxAccelerationMetersPerSecondSquared = kMaxVelocityMetersPerSecond * 0.25;
+
+        public static final double kMaxAngularVelocityRadiansPerSecond = kMaxVelocityMetersPerSecond /
+        Math.hypot(kDrivetrainTrackwidthMeters / 2.0, kDrivetrainWheelbaseMeters / 2.0);
+
+        public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI;
+    }
+
     
 }
