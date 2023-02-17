53cd00de0c99dfa64bf6670b6f0b3a9d65baec9f
Ty McKeon <ty.mckeon@icloud.com>
2023-02-17 15:00:47 -0500

    Finished arg parser

---
+++ ChargedUpButtonBox/ButtonBoxSerialReceiver/include/state.h
@@ -0,0 +1,7 @@
+#ifndef STATE_H_
+#define STATE_H_
+#include <string.h>
+
+String *parsedArgs;
+
+#endif // STATE_H_
--- ChargedUpButtonBox/ButtonBoxSerialReceiver/src/test.cpp
+++ ChargedUpButtonBox/ButtonBoxSerialReceiver/src/test.cpp
@@ -1,4 +1,29 @@
 #include <Arduino.h>
+#include <string.h>
+#include <state.h>
+
+enum CommandType
+{
+  NO_TYPE,
+  COMMAND_ONE_LED,
+  COMMAND_ALL_LEDS,
+  MESSAGE_COMMAND,
+};
+
+enum LedStatus
+{
+  LED_OFF,
+  LED_ON,
+  LED_BLINKING,
+};
+
+struct Command
+{
+  CommandType commandType;
+  uint8_t ledId;
+  LedStatus status;
+  String message;
+};
 
 class LED
 {
@@ -20,85 +45,217 @@ public:
   {
     digitalWrite(m_Id, LOW);
   }
+
+  uint8_t getId()
+  {
+    return m_Id;
+  }
 };
 
-/**
- * Splits a string into substrings using a given separator
- *
- * @param string the string to split
- * @param separator the character to split the string at
- *
- * @return an array of substrings
- *
- */
-String *split(String string, char separator)
+class LedGroup
 {
-  if (separator == '\0')
+  LED *m_Leds;
+  size_t m_Count;
+
+public:
+  LedGroup(LED *leds, size_t size)
   {
-    String result[1] = {string};
-    return result;
+    m_Leds = leds;
+    m_Count = size;
   }
 
-  size_t strLength = string.length();
-
-  if (string.length() == 0)
+  void enableAll()
   {
-    String result[0];
-    return result;
-  }
-
-  size_t arrLength = 0;
-
-  for (size_t i = 1; i < strLength - 1; i++)
-  {
-    if (string[i] != separator)
-      continue;
-
-    char prev = string[i - 1];
-    char next = string[i + 1];
-
-    if (prev != separator && next != separator)
-      arrLength++;
-  }
-
-  String result[arrLength];
-
-  size_t lower = 0;
-  size_t pos = 0;
-  for (size_t i = 1; i < strLength; i++)
-  {
-
-    if (string[i - 1] == separator)
+    for (size_t i = 0; i < m_Count; i++)
     {
-      lower++;
-      continue;
+      (m_Leds + i)->enable();
     }
-
-    if (string[i] != separator)
-      continue;
-
-    result[pos++] = string.substring(lower, i - lower);
   }
 
-  return result;
+  void disableAll()
+  {
+    for (size_t i = 0; i < m_Count; i++)
+    {
+      (m_Leds + i)->disable();
+    }
+  }
+
+  void enableLed(uint8_t ledId)
+  {
+    for (size_t i = 0; i < m_Count; i++)
+    {
+      if ((m_Leds + i)->getId() == ledId)
+      {
+        (m_Leds + i)->enable();
+      }
+    }
+  }
+
+  void disableLed(uint8_t ledId)
+  {
+    for (size_t i = 0; i < m_Count; i++)
+    {
+      if ((m_Leds + i)->getId() == ledId)
+      {
+        (m_Leds + i)->disable();
+      }
+    }
+  }
+
+  LED *
+  findLed(uint8_t ledId)
+  {
+    for (size_t i = 0; i < m_Count; i++)
+    {
+      if ((m_Leds + i)->getId() == ledId)
+        return (m_Leds + i);
+    }
+    return NULL;
+  }
+};
+
+int8_t stoi(char *string)
+{
+  char c = *string;
+  size_t length = 0;
+  while (c != '\0')
+  {
+    length++;
+    c = *(string + length);
+  }
+
+  if (length > 2)
+    return -1;
+
+  c = *string;
+  int8_t sum = 0;
+
+  for (size_t i = 0; i < length; i++)
+  {
+    if (c < '0' || c > '9')
+    {
+      return -1;
+    }
+    sum += pow(10, length - 1) * (c - '0');
+  }
+
+  return sum;
+}
+
+void processCommand(Command command)
+{
 }
 
 /**
- * Process a command based on format and commands available in readme file
+ * Parses a command based on format and commands available in readme file
  *
  * @param command the command to process
+ *
+ * @return the command in a Command struct
  */
-void processCommand(String command)
+Command parseCommand(String command)
 {
+  char *str = strupr((char *)command.c_str());
+  char *split;
+  Command cmd;
 
-  // first we need to split the command up into its part
-  // since it will be separated by spaces, we need to split it by spaces
+  split = strtok(str, " ");
+
+  if (split == NULL)
+    return cmd;
+
+  if (strcmp(split, "LED") == 0)
+  {
+    split = strtok(NULL, " ");
+
+    if (split == NULL)
+      return cmd;
+
+    if (strcmp(split, "SET") == 0)
+    {
+      cmd.commandType = COMMAND_ONE_LED;
+
+      split = strtok(NULL, " ");
+      if (split == NULL)
+      {
+        cmd.commandType = NO_TYPE;
+        return cmd;
+      }
+
+      int8_t id = stoi(split);
+      if (id < 0)
+      {
+        cmd.commandType = NO_TYPE;
+        return cmd;
+      }
+
+      cmd.ledId = (uint8_t)id;
+    }
+    else if (strcmp(split, "SETALL") == 0)
+    {
+      cmd.commandType = COMMAND_ALL_LEDS;
+    }
+    else
+    {
+      return cmd;
+    }
+
+    split = strtok(NULL, " ");
+    if (split == NULL)
+    {
+      cmd.commandType = NO_TYPE;
+      return cmd;
+    }
+
+    if (strcmp(split, "ON") == 0)
+    {
+      cmd.status = LED_ON;
+    }
+    else if (strcmp(split, "OFF") == 0)
+    {
+      cmd.status = LED_OFF;
+    }
+    else if (strcmp(split, "BLINK") == 0)
+    {
+      cmd.status = LED_BLINKING;
+    }
+    else
+    {
+      cmd.commandType = NO_TYPE;
+    }
+  }
+  else if (strcmp(split, "MSG"))
+  {
+    cmd.commandType = MESSAGE_COMMAND;
+
+    split = strtok(NULL, " ");
+    if (split == NULL)
+    {
+      cmd.commandType = NO_TYPE;
+      return cmd;
+    }
+
+    cmd.message = split;
+  }
+
+  return cmd;
 }
+
+Command cmd;
 
 void setup()
 {
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  String command = Serial.readStringUntil('\n');
+  cmd = parseCommand(command);
+  Serial.write(cmd.commandType + '0');
+  Serial.write("\n");
+  Serial.write(cmd.ledId + '0');
+  Serial.write("\n");
+  Serial.write(cmd.status + '0');
+  Serial.write("\n");
 }
