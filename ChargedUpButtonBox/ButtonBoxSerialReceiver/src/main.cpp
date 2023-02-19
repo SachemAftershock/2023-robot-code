22c4e9a7c26b0391dcceb173e3e2094d42c9c3d2
JimTripi <JimTripi@users.noreply.github.com>
2023-02-19 17:42:50 -0500

    Simple working tokenizer, start parser, added LCD

--- ChargedUpButtonBox/ButtonBoxSerialReceiver/src/main.cpp
+++ ChargedUpButtonBox/ButtonBoxSerialReceiver/src/main.cpp
@@ -1,157 +1,94 @@
-// #include <Arduino.h>
-// #include <Wire.h>
-// #include "Timer.h"
+#include <Arduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-// Timer t;
-// char ch;
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-// enum systemModeEnum {eReady, eBusy };
-// systemModeEnum currentSystemMode = eReady;
-// systemModeEnum lastSystemMode = eBusy;
-// int commandReceivedLedOnboardArduino = 13;
+String errorMessage= "No Error";
 
-// enum buttonBoxLedsEnum { 
-//   eLedRoboRioBusy = 0,
+enum ledEnum { eIngest, eEject};
 
-//   eLed_Slot1_Cone = 1,
-//   eLed_Slot2_Cube = 2,
-//   eLed_Slot3_Cone = 3,
+struct TokenBlock {
+  String Token1;
+  String Token2;
+  String Token3;
+  String Token4;
+};
 
-//   eLed_Slot4_Cone = 4,
-//   eLed_Slot5_Cube = 5,
-//   eLed_Slot6_Cone = 6,
+// quick and dirty tokenizer
+TokenBlock Tokenize(String inputString) {
+   TokenBlock Tokens;
+   inputString.trim();
 
-//   eLed_Slot7_Cone = 7,
-//   eLed_Slot8_Cube = 8,
-//   eLed_Slot9_Cone = 9,
+  int nextSpace = inputString.indexOf(" ");
+  Tokens.Token1 = inputString.substring(0,nextSpace);
 
-//   eLed_HumanStationLeft = 10,
-//   eLed_HumanStationRight = 11,
+  inputString = inputString.substring(nextSpace);
+  inputString.trim();
+  nextSpace = inputString.indexOf(" ");
+  Tokens.Token2 = inputString.substring(0,nextSpace);
 
-//   eLed_ConeMode = 12,
-//   eLed_CubeMode = 13,
+  inputString = inputString.substring(nextSpace);
+  inputString.trim();
+  nextSpace = inputString.indexOf(" ");
+  Tokens.Token3 = inputString.substring(0,nextSpace);
 
-//   eLed_ElevatorHigh = 14,
-//   eLed_ElevatorMedium = 15,
-//   eLed_ElevatorLow = 16,
-//   eLed_ElevatorFloor = 17,
-//   eLed_ElevatorPark = 18,
+  inputString = inputString.substring(nextSpace);
+  inputString.trim();
+  nextSpace = inputString.indexOf(" ");
+  Tokens.Token4 = inputString.substring(0,nextSpace);  
+  
+  return Tokens;
+}
 
-//   eLed_Ingest = 19,
-//   eLed_Eject = 20
-// };
+void processInputLine(String theInput) {
+  String kCommandLED = "LED";
+  String kCommandMSG = "MSG";
 
-// bool blinkOn = false;
+  Serial.println(theInput);
+  TokenBlock tokens = Tokenize (theInput);
+  Serial.println(":" + tokens.Token1 + ":" + tokens.Token2 + ":" + tokens.Token3 + ":" + tokens.Token4 + ":");
+  if (tokens.Token1.equalsIgnoreCase(kCommandLED)){ 
+      Serial.println("Detected LED command.");
+      if (tokens.Token2.equalsIgnoreCase("ALL")){
+        //TODO make all the LEDs go to setting in Token3.
+      } else {
+        //TODO process for only a single LED specified in Token3
+        int ledAddr = tokens.Token3.toInt();
+      }
+  } else if (tokens.Token1.equalsIgnoreCase(kCommandMSG)){
+      Serial.println("Detected MSG command.");
+      //TODO do work here using the tokens 2 on.
+  } else {
+      Serial.println("Ignoring unrecognized command.");
+  }
+}
 
-// class LED {
-//   uint8_t m_Id;
+void setup()
+{
+  Serial.begin(9600);
+  //Serial.println(Serial.getTimeout());   // Default is 1 second for readStringUntil.
 
-//   public:
-//     LED(uint8_t ledId) {
-//       m_Id = ledId;
-//       pinMode(m_Id, OUTPUT);
-//     }
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  lcd.setCursor(1, 0); // set the cursor to column 1, line 0
+  lcd.print("AFTERSHOCK 263");  // Print a message to the LCD
+  lcd.setCursor(2, 1); // set the cursor to column 2, line 1
+  lcd.print("Charged Up !");  // Print a message to the LCD.
+  delay(3000);
+  lcd.clear();
+}
 
-//     void Enable() {
-//       digitalWrite(m_Id, HIGH);
-//     }
+void loop()
+{
+  String command = Serial.readStringUntil('\n');
+  if (command.length() > 0) {
+    processInputLine(command);
+  }
 
-//     void Disable() {
-//       digitalWrite(m_Id, LOW);
-//     }
-// };
-
-
-
-
-// void toggleCommandArrivedLed(int LightDuration = 50)
-// {
-//   digitalWrite(commandReceivedLedOnboardArduino, HIGH);
-//   delay(LightDuration);
-//   digitalWrite(commandReceivedLedOnboardArduino, LOW);
-//   delay(LightDuration);
-// }
-
-// void processCommand(char theCh) {
-//   Serial.print("Recieved Command ");
-//   Serial.print(theCh);
-//   Serial.print("\n"); 
-//   switch (theCh)
-//   {
-//     case 'r':
-//       currentSystemMode = eReady;
-//       Serial.write("Commanded RoboRIO is ready for next command. \n");
-//       break;
-   
-//     case 'b':
-//       currentSystemMode = eBusy;
-//       Serial.write("Commanded RoboRIO is busy. \n");
-//       break;
-
-//     default:
-//       Serial.write("Commanded not recognized. \n");
-//       break;
-//   }
-
-// }
-
-// void processCurrentMode() {
-//   switch (currentSystemMode) {
-
-//     case eReady:
-//       if (lastSystemMode != eReady) {
-//         digitalWrite(eLedRoboRioBusy,LOW);
-//         lastSystemMode = eReady;
-//       }
-//       break;
-      
-//     case eBusy:
-//       if (lastSystemMode != eBusy) {
-//         digitalWrite(eLedRoboRioBusy,HIGH);
-//         lastSystemMode = eBusy;
-//       }
-//       break;
-            
-//     default:
-//       Serial.print("Unrecognized Mode");
-//       break;
-       
-//   }
-// }
-
-// void receiveEvent(int howMany)
-// {
-//   Serial.write("receiveEvent Invoked. \n");
-//   while (Wire.available()) //when you recieve a byte (through i2c)
-//   {
-//     toggleCommandArrivedLed();
-
-//     unsigned char state = Wire.read();
-//     Serial.write("Received Char: <");
-//     Serial.write(state);
-//     Serial.write("> \n");
-//     //not sure if it should be a normal or unsigned char
-
-//     processCommand(state);
-//   }
-// }
-
-// void setup() {
-//   Wire.begin(1); //communicate on this address
-//   Wire.onReceive(receiveEvent);//when communication is successful,
-//   Serial.begin(9600);          //go to the function
-//   t.every(200, processCurrentMode);
-//   pinMode(commandReceivedLedOnboardArduino, OUTPUT);
-//   toggleCommandArrivedLed();
-//   pinMode(eLedRoboRioBusy, OUTPUT);
-//   Serial.write("Arduino-Slave-I2C-ButtonBoxSerialReceiver Initialization Complete. \n");
-// }
-
-// void loop() {
-//    t.update();
-//   if (Serial.available()) {
-//     ch = Serial.read();
-//     processCommand(ch);
-//   }
-
-// }
+  lcd.setCursor(0, 0); // set the cursor to column 0jjhkjh, line 0
+  lcd.print("Status Busy/Idle");  // Print a message to the LCD  //TODO
+  lcd.setCursor(0, 1); // set the cursor to column 0, line 1
+  lcd.print("ER: ");
+  lcd.println(errorMessage.substring(0,12));
+}
--- ChargedUpButtonBox/ButtonBoxSerialReceiver/src/test.cpp
+++ ChargedUpButtonBox/ButtonBoxSerialReceiver/src/test.cpp
@@ -1,261 +1,261 @@
-#include <Arduino.h>
-#include <string.h>
-#include <state.h>
+// #include <Arduino.h>
+// #include <string.h>
+// #include <state.h>
 
-enum CommandType
-{
-  NO_TYPE,
-  COMMAND_ONE_LED,
-  COMMAND_ALL_LEDS,
-  MESSAGE_COMMAND,
-};
+// enum CommandType
+// {
+//   NO_TYPE,
+//   COMMAND_ONE_LED,
+//   COMMAND_ALL_LEDS,
+//   MESSAGE_COMMAND,
+// };
 
-enum LedStatus
-{
-  LED_OFF,
-  LED_ON,
-  LED_BLINKING,
-};
+// enum LedStatus
+// {
+//   LED_OFF,
+//   LED_ON,
+//   LED_BLINKING,
+// };
 
-struct Command
-{
-  CommandType commandType;
-  uint8_t ledId;
-  LedStatus status;
-  String message;
-};
+// struct Command
+// {
+//   CommandType commandType;
+//   uint8_t ledId;
+//   LedStatus status;
+//   String message;
+// };
 
-class LED
-{
-  uint8_t m_Id;
+// class LED
+// {
+//   uint8_t m_Id;
 
-public:
-  LED(uint8_t ledId)
-  {
-    m_Id = ledId;
-    pinMode(m_Id, OUTPUT);
-  }
+// public:
+//   LED(uint8_t ledId)
+//   {
+//     m_Id = ledId;
+//     pinMode(m_Id, OUTPUT);
+//   }
 
-  void enable()
-  {
-    digitalWrite(m_Id, HIGH);
-  }
+//   void enable()
+//   {
+//     digitalWrite(m_Id, HIGH);
+//   }
 
-  void disable()
-  {
-    digitalWrite(m_Id, LOW);
-  }
+//   void disable()
+//   {
+//     digitalWrite(m_Id, LOW);
+//   }
 
-  uint8_t getId()
-  {
-    return m_Id;
-  }
-};
+//   uint8_t getId()
+//   {
+//     return m_Id;
+//   }
+// };
 
-class LedGroup
-{
-  LED *m_Leds;
-  size_t m_Count;
+// class LedGroup
+// {
+//   LED *m_Leds;
+//   size_t m_Count;
 
-public:
-  LedGroup(LED *leds, size_t size)
-  {
-    m_Leds = leds;
-    m_Count = size;
-  }
+// public:
+//   LedGroup(LED *leds, size_t size)
+//   {
+//     m_Leds = leds;
+//     m_Count = size;
+//   }
 
-  void enableAll()
-  {
-    for (size_t i = 0; i < m_Count; i++)
-    {
-      (m_Leds + i)->enable();
-    }
-  }
+//   void enableAll()
+//   {
+//     for (size_t i = 0; i < m_Count; i++)
+//     {
+//       (m_Leds + i)->enable();
+//     }
+//   }
 
-  void disableAll()
-  {
-    for (size_t i = 0; i < m_Count; i++)
-    {
-      (m_Leds + i)->disable();
-    }
-  }
+//   void disableAll()
+//   {
+//     for (size_t i = 0; i < m_Count; i++)
+//     {
+//       (m_Leds + i)->disable();
+//     }
+//   }
 
-  void enableLed(uint8_t ledId)
-  {
-    for (size_t i = 0; i < m_Count; i++)
-    {
-      if ((m_Leds + i)->getId() == ledId)
-      {
-        (m_Leds + i)->enable();
-      }
-    }
-  }
+//   void enableLed(uint8_t ledId)
+//   {
+//     for (size_t i = 0; i < m_Count; i++)
+//     {
+//       if ((m_Leds + i)->getId() == ledId)
+//       {
+//         (m_Leds + i)->enable();
+//       }
+//     }
+//   }
 
-  void disableLed(uint8_t ledId)
-  {
-    for (size_t i = 0; i < m_Count; i++)
-    {
-      if ((m_Leds + i)->getId() == ledId)
-      {
-        (m_Leds + i)->disable();
-      }
-    }
-  }
+//   void disableLed(uint8_t ledId)
+//   {
+//     for (size_t i = 0; i < m_Count; i++)
+//     {
+//       if ((m_Leds + i)->getId() == ledId)
+//       {
+//         (m_Leds + i)->disable();
+//       }
+//     }
+//   }
 
-  LED *
-  findLed(uint8_t ledId)
-  {
-    for (size_t i = 0; i < m_Count; i++)
-    {
-      if ((m_Leds + i)->getId() == ledId)
-        return (m_Leds + i);
-    }
-    return NULL;
-  }
-};
+//   LED *
+//   findLed(uint8_t ledId)
+//   {
+//     for (size_t i = 0; i < m_Count; i++)
+//     {
+//       if ((m_Leds + i)->getId() == ledId)
+//         return (m_Leds + i);
+//     }
+//     return NULL;
+//   }
+// };
 
-int8_t stoi(char *string)
-{
-  char c = *string;
-  size_t length = 0;
-  while (c != '\0')
-  {
-    length++;
-    c = *(string + length);
-  }
+// int8_t stoi(char *string)
+// {
+//   char c = *string;
+//   size_t length = 0;
+//   while (c != '\0')
+//   {
+//     length++;
+//     c = *(string + length);
+//   }
 
-  if (length > 2)
-    return -1;
+//   if (length > 2)
+//     return -1;
 
-  c = *string;
-  int8_t sum = 0;
+//   c = *string;
+//   int8_t sum = 0;
 
-  for (size_t i = 0; i < length; i++)
-  {
-    if (c < '0' || c > '9')
-    {
-      return -1;
-    }
-    sum += pow(10, length - 1) * (c - '0');
-  }
+//   for (size_t i = 0; i < length; i++)
+//   {
+//     if (c < '0' || c > '9')
+//     {
+//       return -1;
+//     }
+//     sum += pow(10, length - 1) * (c - '0');
+//   }
 
-  return sum;
-}
+//   return sum;
+// }
 
-void processCommand(Command command)
-{
-}
+// void processCommand(Command command)
+// {
+// }
 
-/**
- * Parses a command based on format and commands available in readme file
- *
- * @param command the command to process
- *
- * @return the command in a Command struct
- */
-Command parseCommand(String command)
-{
-  char *str = strupr((char *)command.c_str());
-  char *split;
-  Command cmd;
+// /**
+//  * Parses a command based on format and commands available in readme file
+//  *
+//  * @param command the command to process
+//  *
+//  * @return the command in a Command struct
+//  */
+// Command parseCommand(String command)
+// {
+//   char *str = strupr((char *)command.c_str());
+//   char *split;
+//   Command cmd;
 
-  split = strtok(str, " ");
+//   split = strtok(str, " ");
 
-  if (split == NULL)
-    return cmd;
+//   if (split == NULL)
+//     return cmd;
 
-  if (strcmp(split, "LED") == 0)
-  {
-    split = strtok(NULL, " ");
+//   if (strcmp(split, "LED") == 0)
+//   {
+//     split = strtok(NULL, " ");
 
-    if (split == NULL)
-      return cmd;
+//     if (split == NULL)
+//       return cmd;
 
-    if (strcmp(split, "SET") == 0)
-    {
-      cmd.commandType = COMMAND_ONE_LED;
+//     if (strcmp(split, "SET") == 0)
+//     {
+//       cmd.commandType = COMMAND_ONE_LED;
 
-      split = strtok(NULL, " ");
-      if (split == NULL)
-      {
-        cmd.commandType = NO_TYPE;
-        return cmd;
-      }
+//       split = strtok(NULL, " ");
+//       if (split == NULL)
+//       {
+//         cmd.commandType = NO_TYPE;
+//         return cmd;
+//       }
 
-      int8_t id = stoi(split);
-      if (id < 0)
-      {
-        cmd.commandType = NO_TYPE;
-        return cmd;
-      }
+//       int8_t id = stoi(split);
+//       if (id < 0)
+//       {
+//         cmd.commandType = NO_TYPE;
+//         return cmd;
+//       }
 
-      cmd.ledId = (uint8_t)id;
-    }
-    else if (strcmp(split, "SETALL") == 0)
-    {
-      cmd.commandType = COMMAND_ALL_LEDS;
-    }
-    else
-    {
-      return cmd;
-    }
+//       cmd.ledId = (uint8_t)id;
+//     }
+//     else if (strcmp(split, "SETALL") == 0)
+//     {
+//       cmd.commandType = COMMAND_ALL_LEDS;
+//     }
+//     else
+//     {
+//       return cmd;
+//     }
 
-    split = strtok(NULL, " ");
-    if (split == NULL)
-    {
-      cmd.commandType = NO_TYPE;
-      return cmd;
-    }
+//     split = strtok(NULL, " ");
+//     if (split == NULL)
+//     {
+//       cmd.commandType = NO_TYPE;
+//       return cmd;
+//     }
 
-    if (strcmp(split, "ON") == 0)
-    {
-      cmd.status = LED_ON;
-    }
-    else if (strcmp(split, "OFF") == 0)
-    {
-      cmd.status = LED_OFF;
-    }
-    else if (strcmp(split, "BLINK") == 0)
-    {
-      cmd.status = LED_BLINKING;
-    }
-    else
-    {
-      cmd.commandType = NO_TYPE;
-    }
-  }
-  else if (strcmp(split, "MSG"))
-  {
-    cmd.commandType = MESSAGE_COMMAND;
+//     if (strcmp(split, "ON") == 0)
+//     {
+//       cmd.status = LED_ON;
+//     }
+//     else if (strcmp(split, "OFF") == 0)
+//     {
+//       cmd.status = LED_OFF;
+//     }
+//     else if (strcmp(split, "BLINK") == 0)
+//     {
+//       cmd.status = LED_BLINKING;
+//     }
+//     else
+//     {
+//       cmd.commandType = NO_TYPE;
+//     }
+//   }
+//   else if (strcmp(split, "MSG"))
+//   {
+//     cmd.commandType = MESSAGE_COMMAND;
 
-    split = strtok(NULL, " ");
-    if (split == NULL)
-    {
-      cmd.commandType = NO_TYPE;
-      return cmd;
-    }
+//     split = strtok(NULL, " ");
+//     if (split == NULL)
+//     {
+//       cmd.commandType = NO_TYPE;
+//       return cmd;
+//     }
 
-    cmd.message = split;
-  }
+//     cmd.message = split;
+//   }
 
-  return cmd;
-}
+//   return cmd;
+// }
 
-Command cmd;
+// Command cmd;
 
-void setup()
-{
-  Serial.begin(9600);
-}
+// void setup()
+// {
+//   Serial.begin(9600);
+// }
 
-void loop()
-{
-  String command = Serial.readStringUntil('\n');
-  cmd = parseCommand(command);
-  Serial.write(cmd.commandType + '0');
-  Serial.write("\n");
-  Serial.write(cmd.ledId + '0');
-  Serial.write("\n");
-  Serial.write(cmd.status + '0');
-  Serial.write("\n");
-}
+// void loop()
+// {
+//   String command = Serial.readStringUntil('\n');
+//   cmd = parseCommand(command);
+//   Serial.write(cmd.commandType + '0');
+//   Serial.write("\n");
+//   Serial.write(cmd.ledId + '0');
+//   Serial.write("\n");
+//   Serial.write(cmd.status + '0');
+//   Serial.write("\n");
+// }
