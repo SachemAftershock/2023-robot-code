fff95e7bfb7c7231384a1f87effe9abe32c63bc7
Arhum Mudassir <59671423+arhummudassir@users.noreply.github.com>
2023-02-28 18:14:37 -0500

    slew rate limiting for driving

---
+++ Archimedes/src/main/java/frc/robot/commands/ManualDriveCommand.java
@@ -0,0 +1,54 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+public class ManualDriveCommand extends CommandBase {
+    private final DriveSubsystem m_drivetrainSubsystem;
+
+    private final DoubleSupplier m_translationXSupplier;
+    private final DoubleSupplier m_translationYSupplier;
+    private final DoubleSupplier m_rotationSupplier;
+
+    private final SlewRateLimiter mRateLimiter;
+
+    public ManualDriveCommand(DriveSubsystem drivetrainSubsystem,
+                               DoubleSupplier translationXSupplier,
+                               DoubleSupplier translationYSupplier,
+                               DoubleSupplier rotationSupplier) {
+        this.m_drivetrainSubsystem = drivetrainSubsystem;
+        this.m_translationXSupplier = translationXSupplier;
+        this.m_translationYSupplier = translationYSupplier;
+        this.m_rotationSupplier = rotationSupplier;
+
+        mRateLimiter = new SlewRateLimiter(DriveConstants.kDriveRateLimit);
+
+        addRequirements(drivetrainSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
+        
+        double xSupplier = mRateLimiter.calculate(m_translationXSupplier.getAsDouble());
+        double ySupplier = mRateLimiter.calculate(m_translationYSupplier.getAsDouble());
+
+        //double rotationSupplier = mRateLimiter.calculate(m_rotationSupplier.getAsDouble());
+        //If rate limiting needed for rotations
+        double rotationSupplier = mRateLimiter.calculate(m_rotationSupplier.getAsDouble());
+        
+        m_drivetrainSubsystem.drive(
+            ChassisSpeeds.fromFieldRelativeSpeeds(xSupplier, ySupplier, rotationSupplier, m_drivetrainSubsystem.getGyroscopeRotation())
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+    }
+}
