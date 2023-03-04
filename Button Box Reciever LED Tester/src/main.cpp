8e2349d8ef03299074a56f77c601b63d96972043
Ty McKeon <ty.mckeon@icloud.com>
2023-03-04 12:27:02 -0500

    Update LED Tester

--- Button Box Reciever LED Tester/src/main.cpp
+++ Button Box Reciever LED Tester/src/main.cpp
@@ -1,22 +1,29 @@
 #include <Arduino.h>
-int LED_Start = 1;
-int LED_End = 22;
-void setup() {
+
+uint8_t LED_Start = 3;
+uint8_t LED_End = 24;
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for(int i = LED_Start; i <= LED_End; ++i){
-    pinMode(i,OUTPUT);
+  for (size_t i = LED_Start; i <= LED_End; i++)
+  {
+    pinMode(i, OUTPUT);
   }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  for(int i = LED_Start; i <= LED_End; ++i){
+  for (size_t i = LED_Start; i <= LED_End; i++)
+  {
     digitalWrite(i, HIGH);
   }
   delay(400);
-  for(int i = LED_Start; i <= LED_End; ++i){
-    digitalWrite(i,LOW);
+  for (size_t i = LED_Start; i <= LED_End; i++)
+  {
+    digitalWrite(i, LOW);
   }
   delay(400);
 }
