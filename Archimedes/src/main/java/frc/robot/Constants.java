44195eea052d7044301ced5e6a46417441882549
Arhum Mudassir <59671423+arhummudassir@users.noreply.github.com>
2023-03-16 13:37:18 -0400

    arm min distances

--- Archimedes/src/main/java/frc/robot/Constants.java
+++ Archimedes/src/main/java/frc/robot/Constants.java
@@ -167,6 +167,9 @@ public class Constants {
         public static final double kDt = 0.02;
 
         public static final double kEpsilon = 0.15;
+        public static final double kMinArmBarDistance = 13.0;
+        public static final double kMaxArmBarDistance = 18.0;
+        public static final double kArmSpeedScalingFactor = 0.4;
 
         public static final double kJogSpeed = 0.2;
         public static final double kArmLidarOffset = -3.0;
