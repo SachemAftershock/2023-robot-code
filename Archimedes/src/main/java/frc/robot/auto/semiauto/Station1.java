db240d5a62e6b26cff489707a75332596d1ab156
Mohid Rattu <59671390+MohidTheMan@users.noreply.github.com>
2023-03-17 18:38:00 -0400

    Create Station1.java

---
+++ Archimedes/src/main/java/frc/robot/auto/semiauto/Station1.java
@@ -0,0 +1,48 @@
+package frc.robot.auto.semiauto;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.semiAutoCommand;
+import frc.robot.commands.drive.DriveToWaypointCommand;
+import frc.robot.commands.drive.SetWaypointCommand;
+
+public class Station1 extends SequentialCommandGroup{
+
+    private final DriveSubsystem mDrive; 
+
+    TrajectoryConfig config = new TrajectoryConfig(
+        DriveConstants.kMaxVelocityMetersPerSecond * 0.3,
+        DriveConstants.kMaxAccelerationMetersPerSecondSquared
+    );
+
+    Pose2d startingPose;
+    Pose2d endPose;
+
+    double endX;
+    double endY;
+
+    public Station1(DriveSubsystem drive, int stationNum) {
+        mDrive = drive;
+        if(DriverStation.getAlliance() == Alliance.Red){
+            endX = DriveConstants.kBlueGridXCoordinate;
+            endY = DriveConstants.kGridYCoordinates[8 - stationNum];
+        }else if(DriverStation.getAlliance() == Alliance.Blue){
+            endX = DriveConstants.kRedGridXCoordinate;
+            endY = DriveConstants.kGridYCoordinates[stationNum];
+        }
+        startingPose = mDrive.getPose();
+        endPose = new Pose2d(1.9, .41, new Rotation2d(Math.PI));
+        addCommands(
+            new SetWaypointCommand(endPose, drive)
+        );
+    }
+
+
+    
+}
