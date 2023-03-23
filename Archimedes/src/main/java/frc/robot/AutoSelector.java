98b803ea5e9c51ef3133d8524cd1d21a3fa0010b
Mohid Rattu <59671390+MohidTheMan@users.noreply.github.com>
2023-03-23 06:35:18 -0400

    some more basic autopaths

--- Archimedes/src/main/java/frc/robot/AutoSelector.java
+++ Archimedes/src/main/java/frc/robot/AutoSelector.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.auto.fieldOrientedTrajectoryAuto.*;
+import frc.robot.auto.linearAuto.AutoPathConeLinear;
+import frc.robot.auto.linearAuto.AutoPathCubeLinear;
 import frc.robot.commands.drive.LinearDriveCommand;
 import frc.robot.commands.drive.RotateDriveCommand;
 import frc.robot.subsystems.ArmSubsystem;
@@ -27,7 +29,8 @@ public class AutoSelector {
      */
     enum AutoPath {
         eNothing, ePath0, eCone, ePath0NC, ePath1, eCube, ePath1NC, ePath2, ePath2NC,
-         ePath3, ePath3NC, ePath4, ePath4NC, ePath5, ePath5NC;
+         ePath3, ePath3NC, ePath4, ePath4NC, ePath5, ePath5NC,
+         eCubeTaxi, eConeTaxi;
     }
 
     private AutoPath mSelectedAutoScenario, mPrevAutoScenario;
@@ -56,6 +59,8 @@ public class AutoSelector {
         mAutoChooser.addOption("Path 4 No Charge", AutoPath.ePath4NC);
         mAutoChooser.addOption("Path 5", AutoPath.ePath5);
         mAutoChooser.addOption("Path 5 No Charge", AutoPath.ePath5NC);
+        mAutoChooser.addOption("Cube Taxi", AutoPath.eCubeTaxi);
+        mAutoChooser.addOption("Cone Taxi", AutoPath.eConeTaxi);
         SmartDashboard.putData("Auto Path", mAutoChooser);
     }
 
@@ -105,6 +110,10 @@ public class AutoSelector {
                 return new AutoPathCone(DriveSubsystem.getInstance(), ElevatorSubsystem.getInstance(), ArmSubsystem.getInstance(), IntakeSubsystem.getInstance());
             case eCube:
                 return new AutoPathCube(DriveSubsystem.getInstance(), ElevatorSubsystem.getInstance(), ArmSubsystem.getInstance(), IntakeSubsystem.getInstance());
+            case eCubeTaxi:
+                return new AutoPathCubeLinear(DriveSubsystem.getInstance(), ElevatorSubsystem.getInstance(), ArmSubsystem.getInstance(), IntakeSubsystem.getInstance());
+            case eConeTaxi:
+                return new AutoPathConeLinear(DriveSubsystem.getInstance(), ElevatorSubsystem.getInstance(), ArmSubsystem.getInstance(), IntakeSubsystem.getInstance());
             case eNothing:
             default: 
                 return null;
